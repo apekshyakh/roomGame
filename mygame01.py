a77860dc3796449547330e88575847b610546b71
Apekshya Khanal <apekshyakh@gmail.com>
2021-01-08 23:16:10 +0000

    initial commit

---
+++ mygame01.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python3
+
+# Replace RPG starter project with this code when new instructions are live
+
+# --------------- AUTHORS: Apekshya and Abelardo -------------------------
+
+def showInstructions():
+  #print a main menu and the commands
+  print('''
+RPG Game
+========
+Commands:
+  go [direction]
+  get [item]
+''')
+
+def showStatus():
+  #print the player's current status
+  print('---------------------------')
+  print('You are in the ' + currentRoom)
+  #print the current inventory
+  print('Inventory : ' + str(inventory))
+  #print an item if there is one
+  if "item" in rooms[currentRoom]:
+    print('You see a ' + rooms[currentRoom]['item'])
+  print("---------------------------")
+
+def combat(item,currentRoom):
+    if item == "lamp":
+        print("You threw the lamp at the monster. He caught on fire!! ")
+        del rooms[currentRoom]['item'] #removing the monster
+    if item == "key":
+        print("You threw the key at the monster. Hint: You needed that !!")
+        #We will add ability to retrieve the item
+    if item == "potion":
+        print("You threw the potion at the monster. He is stronger than before.")
+    inventory.remove(item) 
+
+
+
+
+#an inventory, which is initially empty
+inventory = []
+
+#a dictionary linking a room to other rooms
+## A dictionary linking a room to other rooms
+## A dictionary linking a room to other rooms
+rooms = {
+
+            'Hall' : { 
+                  'south' : 'Kitchen',
+                  'east'  : 'Dining Room',
+                  'west'  : 'Living Room' ,
+                  'north' : 'Master Bedroom',
+                  'item'  : 'key'
+                },
+
+            'Kitchen' : {
+                  'north' : 'Hall',
+                  'item'  : 'monster',
+                },
+            'Dining Room' : {
+                  'west' : 'Hall',
+                  'south': 'Garden',
+                  'item' : 'potion'
+               },
+            'Garden' : {
+                  'north' : 'Dining Room'
+            },
+            'Master Bedroom' : {
+                'south'  : 'Hall',
+                'item'   : 'lamp'
+                }
+            
+         }
+
+
+#start the player in the Hall
+currentRoom = 'Hall'
+
+showInstructions()
+
+#loop forever
+while True:
+
+  showStatus()
+
+  #get the player's next 'move'
+  #.split() breaks it up into an list array
+  #eg typing 'go east' would give the list:
+  #['go','east']
+  move = ''
+  while move == '':  
+    move = input('>')
+  
+  # split allows an items to have a space on them
+  # get golden key is returned ["get", "golden key"]          
+  move = move.lower().split(" ", 1)
+
+  #if they type 'go' first
+  if move[0] == 'go':
+    #check that they are allowed wherever they want to go
+    if move[1] in rooms[currentRoom]:
+      #set the current room to the new room
+      currentRoom = rooms[currentRoom][move[1]]
+    #there is no door (link) to the new room
+    else:
+        print('You can\'t go that way!')
+
+  #if they type 'get' first
+  if move[0] == 'get' :
+    #if the room contains an item, and the item is the one they want to get
+    if "item" in rooms[currentRoom] and move[1] in rooms[currentRoom]['item']:
+      #add the item to their inventory
+      inventory += [move[1]]
+      #display a helpful message
+      print(move[1] + ' got!')
+      #delete the item from the room
+      del rooms[currentRoom]['item']
+    #otherwise, if the item isn't there to get
+    else:
+      #tell them they can't get it
+      print('Can\'t get ' + move[1] + '!')
+
+
+
+    ## If a player enters a room with a monster
+  if 'item' in rooms[currentRoom] and 'monster' in rooms[currentRoom]['item']:
+      showStatus()
+      if len(inventory)>0:
+          def attack():
+
+              userInput = input("What item do you want to choose to attack the monster? ")
+              if userInput in inventory:
+                  combat(userInput,currentRoom)
+              else:
+                  print(userInput + "is not in the inventory! ")
+          def attackAgain():
+              attackAgain = input("Would you like to attack again? ").strip().lower()
+              if attackAgain == "y" or attackAgain == "yes":
+                  attack()
+              elif attackAgain == "n" or attackAgain == "no":
+                  print("You chose not to attack !")
+                 
+              else:
+                   print("Invalid entry")
+                   attackAgain()
+
+          attack()
+          attackAgain()
+
+
+      else:
+          print('A monster has got you... GAME OVER!')
+          break
+
+## Define how a player can win:
+  if currentRoom == 'Garden' and 'key' in inventory and 'potion' in inventory:
+    print('You escaped the house with the ultra rare key and magic potion... YOU WIN!')
+    break
+
